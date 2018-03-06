a7b0ab0c90703b785b53d9797c471e0b3a74a3a0
nour <nour.saffaf@gmail.com>
2018-03-06 08:40:27 +0200

    Initial Commit

---
+++ sawtti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sawtti
+//
+//  Created by Nour on 06/03/2018.
+//  Copyright © 2018 Nour Saffaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
